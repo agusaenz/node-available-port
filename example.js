a6694b46ecb65f549ff19dc0ec3e47d6789c1a17
alsaenz <157411527+agusaenz@users.noreply.github.com>
2024-08-15 10:57:12 -0300

    upload files

---
+++ example.js
@@ -0,0 +1,17 @@
+const http = require('node:http')
+const { findAvailablePort } = require('./find-free-port.js')
+
+console.log(process.env)
+
+const desiredPort = process.env.PORT ?? 3000
+
+const server = http.createServer((req, res) => {
+  console.log('request received')
+  res.end('finished')
+})
+
+findAvailablePort(desiredPort).then(port => {
+  server.listen(port, () => {
+    console.log(`server listening on port http://localhost:${port}`)
+  })
+})
